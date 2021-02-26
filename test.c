73daed1fbb5c3c101ed76c8b225256a4d1298b65
f35 <f35idk@gmail.com>
2021-02-26 22:19:33 +0100

    simplify test script
    don't parse the .cmp file column headers to determine input/output labels - just pass the names of the SPICE netlist inputs/outputs directly to the 'test()' function

--- test.c
+++ test.c
@@ -43,33 +43,16 @@ test(
     const char *netlist_path,
     // the path to the '.cmp' file
     const char *cmp_file_path,
-    // the labels of the columns in the cmp file representing the various
-    // logic inputs/outputs (the cmp file is essentially a truth table)
-    const char *input_labels[],
-    const char *output_labels[]
+    // the labels of the SPICE nodes or voltage
+    // sources representing logic inputs/outputs
+    const char *spice_input_labels[],
+    const char *spice_output_labels[]
 )
 {
-    #define MAX_LABELS 0x100
-    #define MAX_LABEL_LEN 6
-
-    // the labels of the SPICE nodes or voltage sources representing logic
-    // inputs/outputs (corresponding to the labels in the 'input_labels'
-    // and 'output_labels' arrays above)
-    char spice_output_labels[MAX_LABELS][MAX_LABEL_LEN + 1] = {0};
-    char spice_input_labels[MAX_LABELS][MAX_LABEL_LEN + 1] = {0};
-
     int cmp_fd;
     int cmp_file_len;
     int cmp_file_pos;
     char *cmp_file;
-
-    for (int i = 0; input_labels[i]; i++) {
-        assert(strlen(input_labels[i]) <= MAX_LABEL_LEN);
-    }
-
-    for (int i = 0; output_labels[i]; i++) {
-        assert(strlen(output_labels[i]) <= MAX_LABEL_LEN);
-    }
 
     printf("simulating '%s'\n", netlist_path);
 
@@ -79,60 +62,9 @@ test(
     cmp_file = mmap(NULL, cmp_file_len, PROT_READ, MAP_PRIVATE, cmp_fd, 0);
     cmp_file_pos = 0;
 
-    // loop over the labels found in each cell of the first row in the cmp
-    // file. prepend a 'v' to the labels representing a logic input and an
-    // 'n' to the labels representing a logic output, and store them in
-    // 'spice_output_labels' and 'spice_input_labels'. the new labels should
-    // match the labels of the corresponding logic input/output nodes/voltage
-    // sources in the SPICE netlist we're simulating.
-    for (int j = 0, k = 0; cmp_file_pos < cmp_file_len && cmp_file[cmp_file_pos] != '\n';) {
-        char *label = NULL;
-        int label_len = 0;
-
-        // increment 'cmp_file_pos' until a label (or newline) is found
-        while (cmp_file_pos < cmp_file_len
-               && (cmp_file[cmp_file_pos] == ' '
-                   || cmp_file[cmp_file_pos] == '|'
-                   || cmp_file[cmp_file_pos] == '\r')) {
-            cmp_file_pos++;
-        }
-
-        // increment 'label_len' and 'cmp_file_pos' until end of label is reached
-        while (cmp_file_pos < cmp_file_len
-               && cmp_file[cmp_file_pos] != ' '
-               && cmp_file[cmp_file_pos] != '|'
-               && cmp_file[cmp_file_pos] != '\n') {
-            label_len++;
-            cmp_file_pos++;
-        }
-
-        assert(label_len < MAX_LABEL_LEN);
-
-        if (!label_len) {
-            // no valid label found - end of line reached
-            assert(cmp_file[cmp_file_pos] == '\n');
-            break;
-        }
-
-        label = &cmp_file[cmp_file_pos - label_len];
-        for (int h = 0; input_labels[h] || output_labels[h]; h++) {
-            if (input_labels[h] && (int) strlen(input_labels[h]) == label_len
-                && !strncmp(label, input_labels[h], label_len)) {
-                spice_input_labels[j][0] = 'v';
-                spice_input_labels[j][MAX_LABEL_LEN] = 0;
-                strncpy(&spice_input_labels[j][1], label, label_len);
-                j++;
-                break;
-            } else if (output_labels[h] && (int) strlen(output_labels[h]) == label_len
-                       && !strncmp(label, output_labels[h], label_len)) {
-                spice_output_labels[k][0] = 'n';
-                spice_output_labels[k][MAX_LABEL_LEN] = 0;
-                strncpy(&spice_output_labels[k][1], label, label_len);
-                k++;
-                break;
-            }
-        }
-    }
+    // skip first line of file (column headers)
+    while (cmp_file[cmp_file_pos++] != '\n')
+        continue;
 
     int cmd_len = 7 + strlen(netlist_path) + 1;
     char *source_cmd = calloc(sizeof(char), cmd_len);
@@ -146,20 +78,19 @@ test(
         exit(1);
     }
 
-    // TODO: fix this mess
     // simulate and test the logic inputs/outputs, row by row
     while (++cmp_file_pos < cmp_file_len) {
         // loop over every logic input value in the current row of the cmp file
-        for (int j = 0; j < MAX_LABELS && spice_input_labels[j][0]; cmp_file_pos++) {
+        for (int j = 0; spice_input_labels[j]; cmp_file_pos++) {
             // if the current character is ascii zero or one
             if (cmp_file[cmp_file_pos] == '0' || cmp_file[cmp_file_pos] == '1') {
-                char alter_cmd[10 + MAX_LABEL_LEN + 1] = {0};
+                int cmd_len = 10 + strlen(spice_input_labels[j]) + 1;
+                char *alter_cmd = calloc(sizeof(char), cmd_len);
                 char input_voltage = cmp_file[cmp_file_pos] == '1' ? '5' : '0';
 
                 // format the 'alter' command to set the right input voltage
-                snprintf(alter_cmd, sizeof(alter_cmd),
-                         "alter %s = %c", spice_input_labels[j],
-                         input_voltage);
+                snprintf(alter_cmd, cmd_len, "alter %s = %c",
+                         spice_input_labels[j], input_voltage);
 
                 // send the alter command to ngspice
                 int error = ngSpice_Command(alter_cmd);
@@ -167,9 +98,11 @@ test(
 
                 if (error) {
                     printf("error when sending command: '%s'\n", alter_cmd);
+                    free(alter_cmd);
                     exit(1);
                 }
 
+                free(alter_cmd);
                 j++;
             }
         }
@@ -187,8 +120,8 @@ test(
         // loop over every logic output value in the current
         // row of the cmp file (until end of row is reached)
         for (int j = 0; cmp_file[cmp_file_pos] != '\n'; cmp_file_pos++) {
-            if ((cmp_file[cmp_file_pos] == '0' || cmp_file[cmp_file_pos] == '1')
-                && j < MAX_LABELS && spice_output_labels[j][0]) {
+            if (spice_output_labels[j] && (cmp_file[cmp_file_pos] == '0'
+                                           || cmp_file[cmp_file_pos] == '1')) {
                 vector_info *output_info;
                 float output_voltage;
 
@@ -236,58 +169,58 @@ main(void)
 
     test("not/not.sp",
          "not/Not.cmp",
-         (const char *[]) { "in", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "vin", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("nand/nand.sp",
          "nand/Nand.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("nor/nor.sp",
          "nor/Nor.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("and/and.sp",
          "and/And.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("or/or.sp",
          "or/Or.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("xor/xor.sp",
          "xor/Xor.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("mux/mux.sp",
          "mux/Mux.cmp",
-         (const char *[]) { "a", "b", "sel", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", "vsel", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("dmux/dmux.sp",
          "dmux/DMux.cmp",
-         (const char *[]) { "in", "sel", NULL },
-         (const char *[]) { "a", "b", NULL });
+         (const char *[]) { "vin", "vsel", NULL },
+         (const char *[]) { "na", "nb", NULL });
 
     test("4waymux/4waymux.sp",
          "4waymux/4WayMux.cmp",
-         (const char *[]) { "a", "b", "c", "d", "sel0", "sel1", NULL },
-         (const char *[]) { "out", NULL });
+         (const char *[]) { "va", "vb", "vc", "vd", "vsel0", "vsel1", NULL },
+         (const char *[]) { "nout", NULL });
 
     test("halfadder/halfadder.sp",
          "halfadder/HalfAdder.cmp",
-         (const char *[]) { "a", "b", NULL },
-         (const char *[]) { "sum", "carry", NULL });
+         (const char *[]) { "va", "vb", NULL },
+         (const char *[]) { "nsum", "ncarry", NULL });
 
     test("fulladder/fulladder.sp",
          "fulladder/FullAdder.cmp",
-         (const char *[]) { "a", "b", "c", NULL },
-         (const char *[]) { "sum", "carry", NULL });
+         (const char *[]) { "va", "vb", "vc", NULL },
+         (const char *[]) { "nsum", "ncarry", NULL });
 
     return 0;
 }
