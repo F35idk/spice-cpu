1d4bba1f881653dc64ab400e935d3d2bf726fa77
f35 <f35idk@gmail.com>
2021-06-11 13:19:16 +0200

    fix typo in decode rom code

--- write_decode_rom.c
+++ write_decode_rom.c
@@ -68,7 +68,7 @@ main(void)
         [0b1011] = NOT_X | NOT_Y | NOT_OUT | ALU_OUT | FLAGS_IN | X_IN, // OR
         [0b1100] = ADD | NOT_X | NOT_OUT | FLAGS_IN, // CMP
         [0b1101] = IR_OUT | BRANCH_ZR, // BEQ
-        [0b1110] = IR_OUT, BRANCH_CARRY, // BGE
+        [0b1110] = IR_OUT | BRANCH_CARRY, // BGE
         [0b1111] = IR_OUT | PC_IN, // JMP
     };
 
