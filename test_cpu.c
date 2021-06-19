9ef4b2587e942b73b9388752d8b6ed53fe0bff6e
f35 <f35idk@gmail.com>
2021-06-19 22:09:16 +0200

    add cpu program for testing AND + RAM load/store

--- test_cpu.c
+++ test_cpu.c
@@ -7,6 +7,7 @@
 #include "cpu_state.h"
 #include "emulate.h"
 #include "simulate.h"
+#include "instruction.h"
 #include "rom.h"
 
 static void
@@ -31,6 +32,37 @@ test_cpu(const unsigned char (*rom)[16], int n_cycles)
 int
 main(void)
 {
-    test_cpu(&ROM, 33);
+    // code to compute the gcd of 36 and 12
+    const unsigned char rom_1[16] = {
+        LDROM_X(0xe),  // load 12 into x
+        LDROM_Y(0xf),  // load 36 into y
+        [2] = CMP,
+        BEQ(2),        // branch back to rom[2] if x and y are equal
+        BLT(7),        // go to rom[7] if x < y
+        SUB_X_Y,       // x = x - y
+        JMP(2),        // go to rom[2]
+        [7] = SUB_Y_X, // y = y - x
+        JMP(2),        // go to rom[2]
+        [0xe] = 12,
+        [0xf] = 36,
+    };
+
+    // AND, STRAM and LDRAM testing
+    const unsigned char rom_2[16] = {
+        LDROM_X(0xe),
+        LDROM_Y(0xf),
+        AND,
+        BEQ(0),
+        STRAM_X(0),
+        STRAM_Y(1),
+        LDRAM_X(1),
+        LDRAM_Y(0),
+        [0xe] = 0xff,
+        [0xf] = 0xcd,
+    };
+
+    test_cpu(&rom_1, 33);
+    test_cpu(&rom_2, 24);
+
     return 0;
 }
